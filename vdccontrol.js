1da1e652c397521bc4eb6513a303ad6de21e643b
Karim Ali <karim@codescalers.com>
2016-05-19 14:16:46 +0200

    Create vdccontrol_init function & remove GIG header

--- vdccontrol.js
+++ vdccontrol.js
@@ -1,8 +1,18 @@
 'use strict';
 
-var vdccontrol = { // jshint ignore:line
-  json_web_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.qQSekbR5BFKQPc3_7gUiDY6Q9y7RojKzvBTLJ9jGtec',
-  apiurl: '/restmachine/cloudapi',
-  vdc_id: 12,
-  parentelement: $("#vdccontrolparent")
-};
+var vdccontrol = {};
+function vdccontrol_init(json_web_token, apiurl, vdc_id, parentelement) {
+  vdccontrol = {
+    json_web_token: json_web_token,
+    apiurl: apiurl,
+    vdc_id: vdc_id,
+    parentelement: parentelement
+  };
+}
+
+// demo values, we need to feed vdccontrol with config values throw vdccontrol_init()
+// var json_web_token  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.qQSekbR5BFKQPc3_7gUiDY6Q9y7RojKzvBTLJ9jGtec';
+// var apiurl = '/restmachine/cloudapi';
+// var vdc_id = 12;
+// var parentelement = $("#vdccontrolparent");
+// vdccontrol_init(json_web_token, apiurl, vdc_id, parentelement);
